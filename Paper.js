8dcb9deff5df2512b128175df9fedb4235044241
PriyanshuKashyap <74148663+PriyanshuKashyap@users.noreply.github.com>
2021-01-02 12:17:58 -0500

    Update Paper.js

--- Paper.js
+++ Paper.js
@@ -10,7 +10,7 @@ class Paper {
         this.radius = 70;
 
         this.body = Bodies.circle(25, height - 10, this.radius, options);
-        this.img = loadImage("https://projectsassets.s3.us-east-2.amazonaws.com/paper.png");//An error occured here
+        this.img = loadImage("paper.png");//An error occured here
         //https://projectsassets.s3.us-east-2.amazonaws.com/dustbingreen.png
         //https://projectsassets.s3.us-east-2.amazonaws.com/paper.png
         World.add(world, this.body);
@@ -21,15 +21,8 @@ class Paper {
     display() {
         fill("rgb(255, 0, 255)");
         //push();
-        ellipse(this.body.position.x, this.body.position.y, 50, 50);
-        //image(this.img, this.body.position.x, this.body.position.y, 50, 50);
+        //ellipse(this.body.position.x, this.body.position.y, 50, 50);
+        image(this.img, this.body.position.x, this.body.position.y + 27, 50, 50);
         //pop();
     }
 }
-/*
-Issues(1):
-When the "paper image" link is clicked, a big black empty screen will be seen. Therefore, please verify the assigned paper image link for this project.
-When the image() command is used, the paper does not change its appearance.
-*/
-
-//Problems(1): The radius of the paper object inhibits the paper's force.
